1650dde0b0f9371814dbfa823a28f22102083c51
Jaime <jaimesof82@gmail.com>
2020-05-13 20:58:29 -0700

    my version complete with styles and logic

---
+++ dice-game/app.js
@@ -0,0 +1,21 @@
+const rollBtn = document.querySelector('button');
+let rollText1 = document.querySelector('.roll-text-1');
+let rollText2 = document.querySelector('.roll-text-2');
+const diceImg1 = document.querySelector('.dice-img-1');
+const diceImg2 = document.querySelector('.dice-img-2');
+
+
+rollBtn.addEventListener('click', rollBothDice);
+
+function rollDice() {
+  return Math.floor((Math.random() * 6) + 1);
+}
+
+function rollBothDice() {
+  rollText1 = rollDice();
+  rollText2 = rollDice();
+  if (rollText1, rollText2) {
+    diceImg1.style.backgroundImage = `url('dice-images/${rollText1}.png')`;
+    diceImg2.style.backgroundImage = `url('dice-images/${rollText2}.png')`;
+  }
+}
