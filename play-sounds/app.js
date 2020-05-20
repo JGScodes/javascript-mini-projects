4f0f88ead763ff99031bf5598d024761ca075bf5
Jaime <jaimesof82@gmail.com>
2020-05-19 22:24:06 -0700

    Added the make sound function, ui for animals

--- play-sounds/app.js
+++ play-sounds/app.js
@@ -1,8 +1,32 @@
-const button = document.querySelector('button');
-const bark = new Audio('sound/bark.mp3');
-button.addEventListener('click', function () {
-  let duration = bark.duration;
-  let muted = bark.muted;
-  bark.play();
-  console.log(muted);
-})
+const animals = document.querySelectorAll('.animal');
+
+for (let i = 0; i < animals.length; i++) {
+  animals[i].addEventListener('click', function () {
+    let animal = this.innerHTML;
+    let lowerAnimal = animal.toLowerCase();
+    makeSound(lowerAnimal);
+  });
+}
+
+function makeSound(name) {
+  console.table(name);
+  switch (name) {
+    case 'lion': {
+      let sound1 = new Audio('sound/lion.mp3');
+      sound1.play();
+      break;
+    }
+
+    case 'cougar': {
+      let sound2 = new Audio('sound/cougar.mp3');
+      sound2.play();
+      break;
+    }
+
+    case 'dog': {
+      let sound3 = new Audio('sound/bark.mp3');
+      sound3.play();
+      break;
+    }
+  }
+}
