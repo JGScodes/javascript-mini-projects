12c6c5769271de15ca0d402e1847f44bb7e81d6d
Jaime <jaimesof82@gmail.com>
2020-05-19 22:52:21 -0700

    refactored the addStyle and makeSound functions into playIt function

--- play-sounds/app.js
+++ play-sounds/app.js
@@ -4,29 +4,52 @@ for (let i = 0; i < animals.length; i++) {
   animals[i].addEventListener('click', function () {
     let animal = this.innerHTML;
     let lowerAnimal = animal.toLowerCase();
-    makeSound(lowerAnimal);
+    // makeSound(lowerAnimal);
+    // addStyle(lowerAnimal);
+    playIt(lowerAnimal);
   });
 }
 
-function makeSound(name) {
-  console.table(name);
-  switch (name) {
-    case 'lion': {
-      let sound1 = new Audio('sound/lion.mp3');
-      sound1.play();
-      break;
-    }
+// function makeSound(name) {
+//   // console.table(name);
+//   switch (name) {
+//     case 'lion': {
+//       let sound1 = new Audio('sound/lion.mp3');
+//       sound1.play();
+//       break;
+//     }
 
-    case 'cougar': {
-      let sound2 = new Audio('sound/cougar.mp3');
-      sound2.play();
-      break;
-    }
+//     case 'cougar': {
+//       let sound2 = new Audio('sound/cougar.mp3');
+//       sound2.play();
+//       break;
+//     }
 
-    case 'dog': {
-      let sound3 = new Audio('sound/bark.mp3');
-      sound3.play();
-      break;
-    }
-  }
+//     case 'dog': {
+//       let sound3 = new Audio('sound/bark.mp3');
+//       sound3.play();
+//       break;
+//     }
+//   }
+// }
+
+function playIt(name) {
+  let activeEle = document.querySelector('.' + name);
+  let sound1 = new Audio('sound/' + name + '.mp3');
+  sound1.play();
+  activeEle.classList.add('active');
+  setTimeout(function () {
+    activeEle.classList.remove('active');
+  }, 1000);
+  console.log(activeEle);
 }
+
+
+// function addStyle(name) {
+//   let activeEle = document.querySelector('.' + name);
+//   activeEle.classList.add('active');
+//   setTimeout(function () {
+//     activeEle.classList.remove('active');
+//   }, 1000);
+//   console.log(activeEle);
+// }
