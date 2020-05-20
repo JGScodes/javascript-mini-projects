c127e0605c242431551a95a22ca715598f8aa0c8
Jaime <jaimesof82@gmail.com>
2020-05-19 23:20:50 -0700

    removed the animal divs from dom and used javascript to create div eleents and prin them to the dom

--- play-sounds/app.js
+++ play-sounds/app.js
@@ -1,15 +1,32 @@
-const animals = document.querySelectorAll('.animal');
+const myArray = ["lion", "cougar", "bark"];
+document.addEventListener("DOMContentLoaded", init);
 
-for (let i = 0; i < animals.length; i++) {
-  animals[i].addEventListener('click', function () {
-    let animal = this.innerHTML;
-    let lowerAnimal = animal.toLowerCase();
-    // makeSound(lowerAnimal);
-    // addStyle(lowerAnimal);
-    playIt(lowerAnimal);
-  });
+function init() {
+  myArray.forEach(function (item) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "animal " + item);
+    div.innerText = item.toUpperCase();
+    div.addEventListener("click", function () {
+      playIt(item);
+    })
+    document.body.appendChild(div);
+  })
 }
 
+// refactored the addStyle and makeSound functions into the playIt function
+function playIt(name) {
+  let activeEle = document.querySelector("." + name);
+  let sound1 = new Audio("sound/" + name + ".mp3");
+  sound1.play();
+  activeEle.classList.add("active");
+  setTimeout(function () {
+    activeEle.classList.remove("active");
+  }, 1000)
+}
+
+
+
+// OLD CODE
 // function makeSound(name) {
 //   // console.table(name);
 //   switch (name) {
@@ -33,16 +50,15 @@ for (let i = 0; i < animals.length; i++) {
 //   }
 // }
 
-function playIt(name) {
-  let activeEle = document.querySelector('.' + name);
-  let sound1 = new Audio('sound/' + name + '.mp3');
-  sound1.play();
-  activeEle.classList.add('active');
-  setTimeout(function () {
-    activeEle.classList.remove('active');
-  }, 1000);
-  console.log(activeEle);
-}
+// for (let i = 0; i < animals.length; i++) {
+//   animals[i].addEventListener('click', function () {
+//     let animal = this.innerHTML;
+//     let lowerAnimal = animal.toLowerCase();
+//     makeSound(lowerAnimal);
+//     addStyle(lowerAnimal);
+//     playIt(lowerAnimal);
+//   });
+// }
 
 
 // function addStyle(name) {
