e373f7068fd5f078ee3dce256a3d2511cc00d959
Jaime <jaimesof82@gmail.com>
2020-05-20 21:31:34 -0700

    Added the start and done button to beging and end game

--- dynamic-typing/app.js
+++ dynamic-typing/app.js
@@ -1,1 +1,25 @@
-console.dir('bankai');
+const wording = ["Do you like JavaScript as much as I do?", "Hope you are having fun this is a simple game you can make.", "Source code is included so you can create your own version of this challenge."];
+let startTime, endTime;
+const message = document.querySelector(".message");
+const playText = document.querySelector("textarea");
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+  console.log(this.innerText);
+  if (this.innerText == "Start") {
+    playText.disabled = false;
+    playGame();
+  } else if (this.innerText == "Done") {
+    playText.disabled = true;
+    button.innerText = "Start";
+  }
+})
+
+function playGame() {
+  let randomNum = Math.floor(Math.random() * wording.length);
+  message.innerText = wording[randomNum];
+  let date = new Date();
+  startTime = date.getTime();
+  console.log(startTime);
+  button.innerText = "Done";
+  console.log(randomNum);
+}
