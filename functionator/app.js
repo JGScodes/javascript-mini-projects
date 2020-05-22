c83de1930f3be2c28163d47a4c498ce511b0714f
Jaime <jaimesof82@gmail.com>
2020-05-21 21:14:09 -0700

    added some functions for adding and removing spans

--- functionator/app.js
+++ functionator/app.js
@@ -1,4 +1,6 @@
 let myBlock;
+let myFunctionList;
+let funcList = [];
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Ready");
   myBlock = document.createElement("div");
@@ -13,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
   myBlock.style.top = '100px';
   myBlock.style.left = '150px';
   document.body.appendChild(myBlock);
+  myFunctionList = document.createElement('div');
+  document.body.appendChild(myFunctionList);
 });
 
 document.addEventListener('keydown', function (e) {
@@ -20,18 +24,62 @@ document.addEventListener('keydown', function (e) {
   let keyC = e.keyCode;
 
   if (keyC === 37) {
-    goLeft();
+    addFunc('left');
   } else if (keyC === 39) {
-    goRight();
+    addFunc('right');
   } else if (keyC === 38) {
-    goUp();
+    addFunc('up');
   } else if (keyC === 40) {
-    goDown();
+    addFunc('down');
   } else if (keyC === 67) {
     myBlock.style.backgroundColor = randomColor();
+  } else if (keyC === 13 || keyC === 32) {
+    mover();
   }
   console.log(e.keyCode);
 });
+
+function mover() {
+  if (funcList.length > 0) {
+    let cur = myBlock.getBoundingClientRect();
+    let el = funcList.shift();
+    console.log(typeof el + ' el');
+    let item = el.textContent.replace('+', '');
+    myFunctionList.removeChild(el);
+    myBlock.innerHTML = 'Move:' + item;
+    if (item === 'left') {
+      myBlock.style.left = cur.left - cur.width + 'px';
+    }
+    if (item === 'right') {
+      myBlock.style.left = cur.left + cur.width + 'px';
+    }
+    if (item === 'up') {
+      myBlock.style.top = cur.top + cur.height + 'px';
+    }
+    if (item === 'down') {
+      myBlock.style.top = cur.left - cur.width + 'px';
+    }
+    setTimeout(mover, 300);
+  }
+}
+
+function addFunc(val) {
+  let span = document.createElement('span');
+  span.textContent = '+' + val;
+  span.style.padding = '10px';
+  span.style.border = '1px solid #ddd';
+  span.addEventListener('mouseover', function () {
+    this.style.backgroundColor = 'red';
+    this.style.color = 'white';
+  });
+  span.addEventListener('mouseout', function () {
+    this.style.backgroundColor = 'white';
+    this.style.color = 'black';
+  });
+  myFunctionList.appendChild(span);
+  funcList.push(span);
+  console.log(funcList);
+}
 
 function randomColor() {
   return '#' + Math.random().toString(16).substr(-6);
